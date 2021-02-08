fea24fe69a83d572262827187c63b2759bc7f43b
Rakshit Bhardwaj <>
2021-02-09 00:15:19 +0530

    Initial commit 2

---
+++ Classes/Randomer.swift
@@ -0,0 +1,16 @@
+//
+//  Randomer.swift
+//  RBRandomGenerator
+//
+//  Created by Rakshit Bhardwaj on 09/02/21.
+//
+
+
+import Foundation
+
+public class randomdatagenerator{
+    
+    public static func string()-> String{
+        return UUID().uuidString
+    }
+}
---
+++ Example/Pods/Target Support Files/RBRandomGenerator/RBRandomGenerator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RBRandomGenerator : NSObject
+@end
+@implementation PodsDummy_RBRandomGenerator
+@end
